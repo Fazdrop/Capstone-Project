9c7c422c2439d943821c80897b0ac44d09bcbbc3
Fazdrop <ipay110033@gmail.com>
2025-05-26 11:20:24 +0700

    fix Division Controller

--- app/Http/Controllers/Admin/DivisionController.php
+++ app/Http/Controllers/Admin/DivisionController.php
@@ -2,10 +2,51 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\Division;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class DivisionController extends Controller
 {
-    //
+    public function index()
+    {
+        $divisions = Division::all(); // var $divisions (jamak)
+        return view('admin.divisions.index', compact('divisions'));
+    }
+
+    public function create()
+    {
+        return view('admin.divisions.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|unique:divisions,name',
+        ]);
+
+        Division::create($validated);
+        return redirect()->route('admin.divisions.index')->with('success', 'Division berhasil dibuat!');
+    }
+
+    public function edit(Division $division)
+    {
+        return view('admin.divisions.edit', compact('division'));
+    }
+
+    public function update(Request $request, Division $division)
+    {
+        $validated = $request->validate([
+            'name' => 'required|unique:divisions,name,' . $division->id,
+        ]);
+
+        $division->update($validated);
+        return redirect()->route('admin.divisions.index')->with('success', 'Division berhasil diupdate!');
+    }
+
+    public function destroy(Division $division)
+    {
+        $division->delete();
+        return redirect()->route('admin.divisions.index')->with('success', 'Division berhasil dihapus!');
+    }
 }
