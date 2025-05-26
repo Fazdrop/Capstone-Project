4401343ddb0fd37656e7b7fd28651aafe7732382
Fazdrop <ipay110033@gmail.com>
2025-05-26 09:02:18 +0700

    menambah dropdown logout

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -20,7 +20,7 @@
 
     <!-- Dropdown Menu -->
     <div id="user-dropdown" class="hidden absolute right-0 mt-2 w-32 bg-white text-black rounded shadow-md z-50 transition duration-200 ease-out">
-      <form method="#" action="#">
+      <form method="POST" action="{{ route('logout') }}">
         @csrf
         <button type="submit"
                 class="w-full text-left px-4 py-2 text-sm hover:bg-gray-100 transition duration-200 cursor-pointer">
